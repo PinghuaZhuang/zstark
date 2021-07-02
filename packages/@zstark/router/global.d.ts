4cc556c36296c23f4c6c8a2e0420569c1e2b1e13
zhuangpinghua <zphua2016@gmail.com>
2021-07-02 17:49:49 +0800

    remove: 删除旧代码. 移动带其他分支.

--- packages/@zstark/router/global.d.ts
+++
@@ -1,1 +0,0 @@
-declare const PKG_VERSION: string
--- packages/@zstark/track/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare const PKG_VERSION: string
-
-declare const plus: {
-  globalEvent: Pick<Document, 'removeEventListener' | 'addEventListener'>;
-}
-
-declare const TRACK: object
